a088dd8abbdc43baa78053653a2f12c6c63ee001
ZORAN\syedo <syedomarzoran@gmail.com>
2022-08-26 16:35:40 +0800

    Add Welcome message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke\nWhat can I do for you?\n\nHope to see you again soon!");
     }
 }
