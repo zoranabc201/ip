616ecc20d3d6f662ce93296fc8bcb08f2826a235
ZORAN\syedo <syedomarzoran@gmail.com>
2022-08-30 21:03:09 +0800

    Add list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,27 @@ public class Duke {
     void printWelcome() {
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
     }
-    void echocommands() {
+    void acceptCommands() {
         Scanner input = new Scanner(System.in);
-        String command = input.nextLine();
-        while(!command.equals("bye")) {
-            System.out.println(command);
+        String command;
+        String commandList[] = new String[100];
+        int noOfCommands = 0;
+        do {
             command = input.nextLine();
+            if(command.equals("list")) {
+                displayList(commandList, noOfCommands);
+                continue;
+            }
+            System.out.println("added: "+command);
+            commandList[noOfCommands++] = command;
         }
+        while(!command.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
+    }
+    void displayList(String[] commandList, int noOfCommands) {
+        for(int i = 0; i < noOfCommands; i++ ) {
+            System.out.println((i+1)+". "+commandList[i]);
+        }
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -21,6 +34,6 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Duke duke = new Duke();
         duke.printWelcome();
-        duke.echocommands();
+        duke.acceptCommands();
     }
 }
