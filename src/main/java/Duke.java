e47ed40a63d1f8149e215e685d811fce3275f31a
ZORAN\syedo <syedomarzoran@gmail.com>
2022-08-30 22:06:56 +0800

    Add mark/unmark tasks. Add Tasks as class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,48 @@
 import java.util.Scanner;
 public class Duke {
+    Tasks taskList[] = new Tasks[100];
     void printWelcome() {
         System.out.println("Hello I'm Duke\nWhat can I do for you?");
     }
     void acceptCommands() {
         Scanner input = new Scanner(System.in);
         String command;
-        String commandList[] = new String[100];
         int noOfCommands = 0;
         do {
             command = input.nextLine();
             if(command.equals("list")) {
-                displayList(commandList, noOfCommands);
+                displayList(noOfCommands);
                 continue;
             }
+            if(command.indexOf(' ') > 0 && command.substring(0,command.indexOf(' ')).equals("mark")) {
+                markTasks(Integer.parseInt(command.substring(command.lastIndexOf(' ') + 1)));
+                continue;
+            }
+            if(command.indexOf(' ') > 0 && command.substring(0,command.indexOf(' ')).equals("unmark")) {
+                unmarkTasks(Integer.parseInt(command.substring(command.lastIndexOf(' ') + 1)));
+                continue;
+            }
+
             System.out.println("added: "+command);
-            commandList[noOfCommands++] = command;
+            taskList[noOfCommands] = new Tasks();
+            taskList[noOfCommands++].setTaskName(command);
         }
         while(!command.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
     }
-    void displayList(String[] commandList, int noOfCommands) {
+    void markTasks(int taskNumber) {
+        System.out.println("Nice! I've marked this task as done:");
+        taskList[taskNumber-1].setDone(true);
+        System.out.println((taskNumber)+". "+ taskList[taskNumber-1].taskDoneStatus()+ taskList[taskNumber-1].getTaskName());
+    }
+    void unmarkTasks(int taskNumber) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        taskList[taskNumber-1].setDone(false);
+        System.out.println((taskNumber)+". "+ taskList[taskNumber-1].taskDoneStatus()+ taskList[taskNumber-1].getTaskName());
+    }
+    void displayList(int noOfCommands) {
         for(int i = 0; i < noOfCommands; i++ ) {
-            System.out.println((i+1)+". "+commandList[i]);
+            System.out.println((i+1)+". "+ taskList[i].taskDoneStatus()+ taskList[i].getTaskName());
         }
     }
     public static void main(String[] args) {
---
+++ src/main/java/Tasks.java
@@ -0,0 +1,28 @@
+public class Tasks {
+    String taskName;
+    boolean done;
+    Tasks() {
+        taskName = "";
+        done = false;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String taskDoneStatus() {
+        if(this.done) {
+            return "[X]";
+        }
+        else {
+            return "[ ]";
+        }
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
